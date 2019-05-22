fb673f2cb51ca384a0c6589cdb50aaac9a2e2310
Gabi <butuzag123@gmail.com>
2019-05-23 00:13:25 +0300

    Heroku settings

--- src/main/resources/db/migration/V1.0.0/V1.0.0_1__create_book_table.sql
+++ src/main/resources/db/migration/V1.0.0/V1.0.0_1__create_book_table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE IF NOT EXISTS `book` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `description` longtext,
-  `author` varchar(255) NOT NULL,
-  `code` varchar(255) NOT NULL,
-  `price` varchar(255) DEFAULT NULL,
+  `title` varchar(255) NOT NULL,
+  `text` longtext,
+  `categoryId` bigint(20) NOT NULL,
   PRIMARY KEY (`id`)
+  CONSTRAINT FK_BookCategory FOREIGN KEY (`categoryId`)
+    REFERENCES category(`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
